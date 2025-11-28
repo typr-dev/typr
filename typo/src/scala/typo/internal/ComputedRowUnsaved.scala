oyvindberg/typo

Correct order of parameters in `RowUnsaved#toRow` without named parameters.
tweak formatting

typo/src/scala/typo/internal/ComputedRowUnsaved.scala
@@ -1,32 +1,50 @@
 package typo
 package internal
 
+case class ComputedRowUnsaved(
+    categorizedColumns: NonEmptyList[ComputedRowUnsaved.CategorizedColumn],
+    categorizedColumnsOriginalOrder: NonEmptyList[ComputedRowUnsaved.CategorizedColumn],
+    defaultCols: NonEmptyList[ComputedRowUnsaved.DefaultedCol],
+    tpe: sc.Type.Qualified
+) {
+  def allCols: NonEmptyList[ComputedColumn] = categorizedColumns.map(_.col)
+  def normalColumns: List[ComputedColumn] =
+    categorizedColumns.toList.collect { case n: ComputedRowUnsaved.NormalCol => n.col }
+  def alwaysGeneratedCols: List[ComputedColumn] =
+    categorizedColumns.toList.collect { case n: ComputedRowUnsaved.AlwaysGeneratedCol => n.col }
+}
+
 object ComputedRowUnsaved {
+  sealed trait CategorizedColumn {
+    def col: ComputedColumn
+  }
+  case class DefaultedCol(col: ComputedColumn, originalType: sc.Type) extends CategorizedColumn
+  case class AlwaysGeneratedCol(col: ComputedColumn) extends CategorizedColumn
+  case class NormalCol(col: ComputedColumn) extends CategorizedColumn
+
   def apply(source: Source, cols: NonEmptyList[ComputedColumn], default: ComputedDefault, naming: Naming): Option[ComputedRowUnsaved] = {
-    val (alwaysGenerated, notAlwaysGenerated) = cols.toList.partition(c => c.dbCol.identity.exists(_.ALWAYS))
-    val (defaultCols, restCols) = notAlwaysGenerated.partition(c => c.dbCol.isDefaulted)
-
-    NonEmptyList.fromList(defaultCols).map { nonEmpty =>
-      val defaultCols = nonEmpty.map { col =>
-        (col.copy(tpe = sc.Type.TApply(default.Defaulted, List(col.tpe))), col.tpe)
+    val categorizedColumns: NonEmptyList[CategorizedColumn] =
+      cols.map {
+        case col if col.dbCol.identity.exists(_.ALWAYS) => AlwaysGeneratedCol(col)
+        case col if col.dbCol.isDefaulted =>
+          DefaultedCol(
+            col = col.copy(tpe = sc.Type.TApply(default.Defaulted, List(col.tpe))),
+            originalType = col.tpe
+          )
+        case col => NormalCol(col)
       }
+    val (normal, rest) = categorizedColumns.toList.partition {
+      case x: NormalCol => true
+      case _            => false
+    }
 
+    NonEmptyList.fromList(rest.collect { case d: DefaultedCol => d }).map { defaulted =>
       new ComputedRowUnsaved(
-        defaultCols = defaultCols,
-        restCols = restCols,
-        alwaysGeneratedCols = alwaysGenerated,
+        categorizedColumns = NonEmptyList.fromList(normal ++ rest).get,
+        categorizedColumnsOriginalOrder = categorizedColumns,
+        defaultCols = defaulted,
         tpe = sc.Type.Qualified(naming.rowUnsaved(source))
       )
     }
   }
-}
-
-case class ComputedRowUnsaved(
-    defaultCols: NonEmptyList[(ComputedColumn, sc.Type)],
-    restCols: List[ComputedColumn],
-    alwaysGeneratedCols: List[ComputedColumn],
-    tpe: sc.Type.Qualified
-) {
-  def allCols: NonEmptyList[ComputedColumn] =
-    restCols ::: defaultCols.map { case (col, _) => col }
 }

typo/src/scala/typo/internal/codegen/DbLibAnorm.scala
@@ -496,11 +496,11 @@ class DbLibAnorm(pkg: sc.QIdent, inlineImplicits: Boolean, default: ComputedDefa
                |$mergeSql.executeUpdate()""".stripMargin
 
       case RepoMethod.InsertUnsaved(relName, cols, unsaved, unsavedParam, default, rowType) =>
-        val cases0 = unsaved.restCols.map { col =>
+        val cases0 = unsaved.normalColumns.map { col =>
           val colCast = sc.StrLit(SqlCast.toPg(col.dbCol).fold("")(_.withColons))
           code"""Some(($NamedParameter(${sc.StrLit(col.dbName.value)}, $ParameterValue(${unsavedParam.name}.${col.name}, null, ${lookupToStatementFor(col.tpe)})), $colCast))"""
         }
-        val cases1 = unsaved.defaultCols.map { case (col @ ComputedColumn(_, ident, _, dbCol), origType) =>
+        val cases1 = unsaved.defaultCols.map { case ComputedRowUnsaved.DefaultedCol(col @ ComputedColumn(_, ident, _, dbCol), origType) =>
           val dbName = sc.StrLit(dbCol.name.value)
           val colCast = sc.StrLit(SqlCast.toPg(col.dbCol).fold("")(_.withColons))
 

typo/src/scala/typo/internal/codegen/DbLibDoobie.scala
@@ -240,11 +240,11 @@ class DbLibDoobie(pkg: sc.QIdent, inlineImplicits: Boolean, default: ComputedDef
                |  .map(_ > 0)"""
 
       case RepoMethod.InsertUnsaved(relName, cols, unsaved, unsavedParam, default, rowType) =>
-        val cases0 = unsaved.restCols.map { col =>
+        val cases0 = unsaved.normalColumns.map { col =>
           val set = frInterpolate(code"${runtimeInterpolateValue(code"${unsavedParam.name}.${col.name}", col.tpe)}${SqlCast.toPgCode(col)}")
           code"""Some(($Fragment.const0(${sc.s(col.dbName.code)}), $set))"""
         }
-        val cases1 = unsaved.defaultCols.map { case (col @ ComputedColumn(_, ident, _, _), origType) =>
+        val cases1 = unsaved.defaultCols.map { case ComputedRowUnsaved.DefaultedCol(col @ ComputedColumn(_, ident, _, _), origType) =>
           val setValue = frInterpolate(code"${runtimeInterpolateValue(code"value: $origType", origType)}${SqlCast.toPgCode(col)}")
           code"""|${unsavedParam.name}.$ident match {
                  |  case ${default.Defaulted}.${default.UseDefault} => None

typo/src/scala/typo/internal/codegen/DbLibZioJdbc.scala
@@ -359,11 +359,11 @@ class DbLibZioJdbc(pkg: sc.QIdent, inlineImplicits: Boolean, dslEnabled: Boolean
                |$sql.update.map(_ > 0)"""
 
       case RepoMethod.InsertUnsaved(relName, cols, unsaved, unsavedParam, default, rowType) =>
-        val cases0 = unsaved.restCols.map { col =>
+        val cases0 = unsaved.normalColumns.map { col =>
           val set = SQL(code"${runtimeInterpolateValue(code"${unsavedParam.name}.${col.name}", col.tpe)}${SqlCast.toPgCode(col)}")
           code"""Some((${SQL(col.dbName)}, $set))"""
         }
-        val cases1 = unsaved.defaultCols.map { case (col @ ComputedColumn(_, ident, _, _), origType) =>
+        val cases1 = unsaved.defaultCols.map { case ComputedRowUnsaved.DefaultedCol(col @ ComputedColumn(_, ident, _, _), origType) =>
           val setValue = SQL(code"${runtimeInterpolateValue(code"value: $origType", origType)}${SqlCast.toPgCode(col)}")
           code"""|${unsavedParam.name}.$ident match {
                  |  case ${default.Defaulted}.${default.UseDefault} => None

typo/src/scala/typo/internal/codegen/FilesRelation.scala
@@ -36,12 +36,10 @@ case class FilesRelation(
           sc.Value(Nil, extractFkId.name.prepended("extract"), Nil, Nil, extractFkId.otherCompositeIdType, body)
         },
         maybeUnsavedRow.map { case (unsaved, defaults) =>
-          val (partOfId, rest) = unsaved.defaultCols.toList.partition { case (col, _) => names.isIdColumn(col.dbName) }
-          val partOfIdParams = partOfId.map { case (col, tpe) =>
-            sc.Param(col.name, defaults.Defaulted.of(tpe), None)
-          }
-          val restParams = rest.map { case (col, tpe) =>
-            sc.Param(col.name, defaults.Defaulted.of(tpe), Some(code"${defaults.Defaulted}.${defaults.Provided}(this.${col.name})"))
+          val (partOfId, rest) = unsaved.defaultCols.toList.partition { case ComputedRowUnsaved.DefaultedCol(col, _) => names.isIdColumn(col.dbName) }
+          val partOfIdParams = partOfId.map { case ComputedRowUnsaved.DefaultedCol(col, _) => sc.Param(col.name, col.tpe, None) }
+          val restParams = rest.map { case ComputedRowUnsaved.DefaultedCol(col, _) =>
+            sc.Param(col.name, col.tpe, Some(code"${defaults.Defaulted}.${defaults.Provided}(this.${col.name})"))
           }
           val params = partOfIdParams ++ restParams
           code"""|def toUnsavedRow(${params.map(_.code).mkCode(", ")}): ${unsaved.tpe} =

typo/src/scala/typo/internal/codegen/FilesTable.scala
@@ -20,30 +20,23 @@ case class FilesTable(table: ComputedTable, fkAnalysis: FkAnalysis, options: Int
           sc.Ident(col.name.value).appended("Default")
 
         val params: NonEmptyList[sc.Param] =
-          unsaved.defaultCols.map { case (col, originalType) => sc.Param(mkDefaultParamName(col), sc.Type.ByName(originalType), None) } ++
+          unsaved.defaultCols.map { case ComputedRowUnsaved.DefaultedCol(col, originalType) => sc.Param(mkDefaultParamName(col), sc.Type.ByName(originalType), None) } ++
             unsaved.alwaysGeneratedCols.map(col => sc.Param(mkDefaultParamName(col), sc.Type.ByName(col.tpe), None))
 
-        val keyValues1 =
-          unsaved.defaultCols.map { case (col, _) =>
+        val keyValues = unsaved.categorizedColumnsOriginalOrder.map {
+          case ComputedRowUnsaved.DefaultedCol(col, defaultType) =>
             val defaultParamName = mkDefaultParamName(col)
             val impl = code"""|${col.name} match {
                    |  case ${table.default.Defaulted}.${table.default.UseDefault} => $defaultParamName
                    |  case ${table.default.Defaulted}.${table.default.Provided}(value) => value
                    |}""".stripMargin
             (col.name, impl)
-          } ++
-            unsaved.alwaysGeneratedCols.map { col =>
-              val defaultParamName = mkDefaultParamName(col)
-              (col.name, defaultParamName.code)
-            }
-
-        val keyValues2 =
-          unsaved.restCols.map { col =>
+          case ComputedRowUnsaved.AlwaysGeneratedCol(col) =>
+            val defaultParamName = mkDefaultParamName(col)
+            (col.name, defaultParamName.code)
+          case ComputedRowUnsaved.NormalCol(col) =>
             (col.name, sc.QIdent.of(col.name).code)
-          }
-
-        val keyValues: List[(sc.Ident, sc.Code)] =
-          keyValues2 ::: keyValues1.toList
+        }
 
         code"""|def toRow(${params.map(_.code).mkCode(", ")}): ${table.names.RowName} =
              |  ${table.names.RowName}(

typr-dev/typr

typo/src/scala/typo/internal/ComputedRowUnsaved.scala
@@ -2,31 +2,60 @@ package typo
 package internal
 
 object ComputedRowUnsaved {
-  def apply(source: Source, allCols: NonEmptyList[ComputedColumn], default: ComputedDefault, naming: Naming): Option[ComputedRowUnsaved] = {
-    val (alwaysGenerated, notAlwaysGenerated) = allCols.toList.partition(c => c.dbCol.maybeGenerated.exists(_.ALWAYS))
-    val (defaultCols, restCols) = notAlwaysGenerated.partition(c => c.dbCol.isDefaulted)
-    val defaultCols2 = defaultCols.map { col =>
-      (col.copy(tpe = sc.Type.TApply(default.Defaulted, List(col.tpe))), col.tpe)
+  sealed trait CategorizedColumn {
+    def col: ComputedColumn
+  }
+  case class DefaultedCol(col: ComputedColumn, originalType: sc.Type) extends CategorizedColumn
+  case class AlwaysGeneratedCol(col: ComputedColumn) extends CategorizedColumn
+  case class NormalCol(col: ComputedColumn) extends CategorizedColumn
+
+  def apply(source: Source, cols: NonEmptyList[ComputedColumn], default: ComputedDefault, naming: Naming): Option[ComputedRowUnsaved] = {
+    val categorizedColumns: NonEmptyList[CategorizedColumn] =
+      cols.map {
+        case col if col.dbCol.maybeGenerated.exists(_.ALWAYS) => AlwaysGeneratedCol(col)
+        case col if col.dbCol.isDefaulted =>
+          DefaultedCol(
+            col = col.copy(tpe = sc.Type.TApply(default.Defaulted, List(col.tpe))),
+            originalType = col.tpe
+          )
+        case col => NormalCol(col)
+      }
+
+    // note: this changes order of columns
+    val unsavedCols: List[CategorizedColumn] =
+      categorizedColumns.toList.collect { case x: NormalCol => x } ++
+        categorizedColumns.toList.collect { case x: DefaultedCol => x }
+
+    val shouldOutputUnsavedRow = categorizedColumns.toList.exists {
+      case _: NormalCol => false
+      case _            => true
     }
-    NonEmptyList.fromList(restCols ::: defaultCols2.map { case (col, _) => col }).collect {
-      case unsavedCols if defaultCols.nonEmpty || alwaysGenerated.nonEmpty =>
+
+    if (shouldOutputUnsavedRow)
+      NonEmptyList.fromList(unsavedCols).map { unsavedCols =>
         new ComputedRowUnsaved(
-          allCols,
-          defaultCols = defaultCols2,
-          restCols = restCols,
-          alwaysGeneratedCols = alwaysGenerated,
-          unsavedCols = unsavedCols,
+          categorizedUnsavedCols = unsavedCols,
+          categorizedColumnsOriginalOrder = categorizedColumns,
           tpe = sc.Type.Qualified(naming.rowUnsaved(source))
         )
-    }
+      }
+    else None
   }
 }
 
 case class ComputedRowUnsaved(
-    allCols: NonEmptyList[ComputedColumn],
-    defaultCols: List[(ComputedColumn, sc.Type)],
-    restCols: List[ComputedColumn],
-    alwaysGeneratedCols: List[ComputedColumn],
-    unsavedCols: NonEmptyList[ComputedColumn],
+    categorizedUnsavedCols: NonEmptyList[ComputedRowUnsaved.CategorizedColumn],
+    categorizedColumnsOriginalOrder: NonEmptyList[ComputedRowUnsaved.CategorizedColumn],
     tpe: sc.Type.Qualified
-)
+) {
+  // all columns which goes into an `UnsavedRow` type
+  def unsavedCols: NonEmptyList[ComputedColumn] =
+    categorizedUnsavedCols.map(_.col)
+
+  def normalColumns: List[ComputedColumn] =
+    categorizedUnsavedCols.toList.collect { case n: ComputedRowUnsaved.NormalCol => n.col }
+  def defaultedCols: List[ComputedRowUnsaved.DefaultedCol] =
+    categorizedUnsavedCols.toList.collect { case d: ComputedRowUnsaved.DefaultedCol => d }
+  def alwaysGeneratedCols: List[ComputedColumn] =
+    categorizedColumnsOriginalOrder.toList.collect { case n: ComputedRowUnsaved.AlwaysGeneratedCol => n.col }
+}

typo/src/scala/typo/internal/codegen/DbLibAnorm.scala
@@ -505,11 +505,11 @@ class DbLibAnorm(pkg: sc.QIdent, inlineImplicits: Boolean, default: ComputedDefa
                |$mergeSql.executeUpdate()""".stripMargin
 
       case RepoMethod.InsertUnsaved(relName, cols, unsaved, unsavedParam, default, rowType) =>
-        val cases0 = unsaved.restCols.map { col =>
+        val cases0 = unsaved.normalColumns.map { col =>
           val colCast = sc.StrLit(SqlCast.toPg(col.dbCol).fold("")(_.withColons))
           code"""Some(($NamedParameter(${sc.StrLit(col.dbName.value)}, $ParameterValue(${unsavedParam.name}.${col.name}, null, ${lookupToStatementFor(col.tpe)})), $colCast))"""
         }
-        val cases1 = unsaved.defaultCols.map { case (col @ ComputedColumn(_, ident, _, dbCol), origType) =>
+        val cases1 = unsaved.defaultedCols.map { case ComputedRowUnsaved.DefaultedCol(col @ ComputedColumn(_, ident, _, dbCol), origType) =>
           val dbName = sc.StrLit(dbCol.name.value)
           val colCast = sc.StrLit(SqlCast.toPg(col.dbCol).fold("")(_.withColons))
 

typo/src/scala/typo/internal/codegen/DbLibDoobie.scala
@@ -238,11 +238,11 @@ class DbLibDoobie(pkg: sc.QIdent, inlineImplicits: Boolean, default: ComputedDef
                |${query(sql, param.tpe)}.option"""
 
       case RepoMethod.InsertUnsaved(relName, cols, unsaved, unsavedParam, default, rowType) =>
-        val cases0 = unsaved.restCols.map { col =>
+        val cases0 = unsaved.normalColumns.map { col =>
           val set = frInterpolate(code"${runtimeInterpolateValue(code"${unsavedParam.name}.${col.name}", col.tpe)}${SqlCast.toPgCode(col)}")
           code"""Some(($Fragment.const0(${sc.s(col.dbName.code)}), $set))"""
         }
-        val cases1 = unsaved.defaultCols.map { case (col @ ComputedColumn(_, ident, _, _), origType) =>
+        val cases1 = unsaved.defaultedCols.map { case ComputedRowUnsaved.DefaultedCol(col @ ComputedColumn(_, ident, _, _), origType) =>
           val setValue = frInterpolate(code"${runtimeInterpolateValue(code"value: $origType", origType)}${SqlCast.toPgCode(col)}")
           code"""|${unsavedParam.name}.$ident match {
                  |  case ${default.Defaulted}.${default.UseDefault} => None

typo/src/scala/typo/internal/codegen/DbLibZioJdbc.scala
@@ -362,11 +362,11 @@ class DbLibZioJdbc(pkg: sc.QIdent, inlineImplicits: Boolean, dslEnabled: Boolean
                |  .selectOne"""
 
       case RepoMethod.InsertUnsaved(relName, cols, unsaved, unsavedParam, default, rowType) =>
-        val cases0 = unsaved.restCols.map { col =>
+        val cases0 = unsaved.normalColumns.map { col =>
           val set = SQL(code"${runtimeInterpolateValue(code"${unsavedParam.name}.${col.name}", col.tpe)}${SqlCast.toPgCode(col)}")
           code"""Some((${SQL(col.dbName)}, $set))"""
         }
-        val cases1 = unsaved.defaultCols.map { case (col @ ComputedColumn(_, ident, _, _), origType) =>
+        val cases1 = unsaved.defaultedCols.map { case ComputedRowUnsaved.DefaultedCol(col @ ComputedColumn(_, ident, _, _), origType) =>
           val setValue = SQL(code"${runtimeInterpolateValue(code"value: $origType", origType)}${SqlCast.toPgCode(col)}")
           code"""|${unsavedParam.name}.$ident match {
                  |  case ${default.Defaulted}.${default.UseDefault} => None

typo/src/scala/typo/internal/codegen/FilesRelation.scala
@@ -37,12 +37,10 @@ case class FilesRelation(
           sc.Value(Nil, extractFkId.name.prepended("extract"), Nil, Nil, extractFkId.otherCompositeIdType, body).code
         },
         maybeUnsavedRow.map { case (unsaved, defaults) =>
-          val (partOfId, rest) = unsaved.defaultCols.toList.partition { case (col, _) => names.isIdColumn(col.dbName) }
-          val partOfIdParams = partOfId.map { case (col, tpe) =>
-            sc.Param(col.name, defaults.Defaulted.of(tpe), None)
-          }
-          val restParams = rest.map { case (col, tpe) =>
-            sc.Param(col.name, defaults.Defaulted.of(tpe), Some(code"${defaults.Defaulted}.${defaults.Provided}(this.${col.name})"))
+          val (partOfId, rest) = unsaved.defaultedCols.toList.partition { case ComputedRowUnsaved.DefaultedCol(col, _) => names.isIdColumn(col.dbName) }
+          val partOfIdParams = partOfId.map { case ComputedRowUnsaved.DefaultedCol(col, _) => sc.Param(col.name, col.tpe, None) }
+          val restParams = rest.map { case ComputedRowUnsaved.DefaultedCol(col, _) =>
+            sc.Param(col.name, col.tpe, Some(code"${defaults.Defaulted}.${defaults.Provided}(this.${col.name})"))
           }
           val params = partOfIdParams ++ restParams
           code"""|def toUnsavedRow(${params.map(_.code).mkCode(", ")}): ${unsaved.tpe} =

typo/src/scala/typo/internal/codegen/FilesTable.scala
@@ -21,30 +21,23 @@ case class FilesTable(table: ComputedTable, fkAnalysis: FkAnalysis, options: Int
           sc.Ident(col.name.value).appended("Default")
 
         val params: List[sc.Param] =
-          unsaved.defaultCols.map { case (col, originalType) => sc.Param(mkDefaultParamName(col), sc.Type.ByName(originalType), None) } ++
+          unsaved.defaultedCols.map { case ComputedRowUnsaved.DefaultedCol(col, originalType) => sc.Param(mkDefaultParamName(col), sc.Type.ByName(originalType), None) } ++
             unsaved.alwaysGeneratedCols.map(col => sc.Param(mkDefaultParamName(col), sc.Type.ByName(col.tpe), None))
 
-        val keyValues1 =
-          unsaved.defaultCols.map { case (col, _) =>
+        val keyValues = unsaved.categorizedColumnsOriginalOrder.map {
+          case ComputedRowUnsaved.DefaultedCol(col, _) =>
             val defaultParamName = mkDefaultParamName(col)
             val impl = code"""|${col.name} match {
                    |  case ${table.default.Defaulted}.${table.default.UseDefault} => $defaultParamName
                    |  case ${table.default.Defaulted}.${table.default.Provided}(value) => value
                    |}""".stripMargin
             (col.name, impl)
-          } ++
-            unsaved.alwaysGeneratedCols.map { col =>
-              val defaultParamName = mkDefaultParamName(col)
-              (col.name, defaultParamName.code)
-            }
-
-        val keyValues2 =
-          unsaved.restCols.map { col =>
+          case ComputedRowUnsaved.AlwaysGeneratedCol(col) =>
+            val defaultParamName = mkDefaultParamName(col)
+            (col.name, defaultParamName.code)
+          case ComputedRowUnsaved.NormalCol(col) =>
             (col.name, sc.QIdent.of(col.name).code)
-          }
-
-        val keyValues: List[(sc.Ident, sc.Code)] =
-          keyValues2 ::: keyValues1.toList
+        }
 
         code"""|def toRow(${params.map(_.code).mkCode(", ")}): ${table.names.RowName} =
              |  ${table.names.RowName}(